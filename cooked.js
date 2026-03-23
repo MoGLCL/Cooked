18dff5576d8415e600242f08dbc2e2531c2a3f7a
MoGlitch <mohamedwaelgl2004@gmail.com>
2026-03-23 09:25:31 +0200

    Update cooked.js

--- cooked.js
+++ cooked.js
@@ -5,5 +5,12 @@ const cooked = [
   "link": {
     "server1": "https://cdn.discordapp.com/attachments/1218402226070880318/1485530837335277659/befores.py?ex=69c233e7&is=69c0e267&hm=a2b3e81c599bf5208ae6ac8607220248c26fddc8a4bc1c9d407e18230a45b37a&"
   }
+},
+{
+  "name": "sabow",
+  "icon": "https://cdn.discordapp.com/attachments/1218402226070880318/1485539397108633672/image.png?ex=69c23be0&is=69c0ea60&hm=52586c0b5a899d0f386282ee9ccfad400afaeedc37fd8b12e5304e5ec3411ef3&",
+  "link": {
+    "server1": "https://cdn.discordapp.com/attachments/1218402226070880318/1485539504046739466/before.py?ex=69c23bf9&is=69c0ea79&hm=1257c632ae3043fc0cecaaeae061501051b93f591fb7c205ef776c3dbfad9ebc&"
+  }
 }
 ];
